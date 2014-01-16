cf74dce345e46f8542f692249a15026f93aed157
Janson Kang <temasek71@gmail.com>
2014-01-16 21:30:12 +0800

    Add screen recorder

--- config/common.mk
+++ config/common.mk
@@ -149,6 +149,11 @@ PRODUCT_PACKAGES += \
     Basic \
     libemoji
 
+# Screen recorder
+PRODUCT_PACKAGES += \
+    ScreenRecorder \
+    libscreenrecorder
+
 # Custom CM packages
 PRODUCT_PACKAGES += \
     Launcher3 \
