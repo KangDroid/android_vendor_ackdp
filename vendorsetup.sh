166a4286347405570f1a588905b7c33fc3131098
KangDroid <KangDroid@KangDroidui-MacBook-Air.local>
2014-12-15 08:25:59 +0000

    Intital KangDroid-Project.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,3 @@
-for combo in $(curl -s https://raw.githubusercontent.com/CyanogenMod/hudson/master/cm-build-targets | sed -e 's/#.*$//' | grep cm-11.0 | awk {'print $1'})
-do
+for combo in $(curl -s https://raw.githubusercontent.com/KangDroid/Vendorsetup/master/ackdp-build-target | sed -e 's/#.*$//' | grep cm-11.0 | awk {'print $1'})do
     add_lunch_combo $combo
 done
